9bbcf9e0574eb84b5c887b4a6414e2dd5f0b66c0
Late <39554860+latelam@users.noreply.github.com>
2018-11-07 13:24:03 +0200

    Add files via upload

---
+++ SOS_late.ino
@@ -0,0 +1,40 @@
+const int PIN = 13;
+
+void setup()
+{
+  pinMode(PIN, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  piste(); 
+  piste(); 
+  piste();
+  viiva(); 
+  viiva(); 
+  viiva();
+  piste(); 
+  piste(); 
+  piste();
+  
+  delay(3000);
+}
+
+void piste()
+{
+  digitalWrite(PIN, HIGH);
+  Serial.println(".");
+  delay(250);
+  digitalWrite(PIN, LOW);
+  delay(250);
+}
+
+void viiva()
+{
+  digitalWrite(PIN, HIGH);
+  Serial.println("-");
+  delay(1000);
+  digitalWrite(PIN, LOW);
+  delay(250);
+}
